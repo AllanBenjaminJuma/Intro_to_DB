7da627b42b479b4683902d5663785cb70244bb0f
allan_Ben <allanbenjuma@gmail.com>
2025-06-29 22:26:14 +0300

    corrected task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -5,4 +5,4 @@ USE alx_book_store;
 
 -- Insert a single row into the Customers table.
 INSERT INTO customer (customer_id, customer_name, email, address)
-VALUES (1, 'Allan Juma', 'allan.juma@aben.co.ke', '123 Kasarani.');
+VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
