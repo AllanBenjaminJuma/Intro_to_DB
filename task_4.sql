b79d14d94dc9289dbac14f56300ff7c6f00f9c42
allan_Ben <allanbenjuma@gmail.com>
2025-06-29 22:18:01 +0300

    added task_4.sql

---
+++ task_4.sql
@@ -0,0 +1,8 @@
+-- task_4.sql
+
+-- Use the alx_book_store database explicitly.
+USE alx_book_store;
+
+-- Prints the full CREATE TABLE statement for the 'books' table,
+-- which includes its full description (columns, types, constraints, etc.).
+SHOW CREATE TABLE Books;
