dc4718933a86e8689e0723b9157b90b25b7b1f3c
allan_Ben <allanbenjuma@gmail.com>
2025-06-29 22:23:10 +0300

    added task_5.sql

---
+++ task_5.sql
@@ -0,0 +1,8 @@
+-- task_5.sql
+
+-- Use the alx_book_store database.
+USE alx_book_store;
+
+-- Insert a single row into the Customers table.
+INSERT INTO Customers (customer_id, customer_name, email, address)
+VALUES (1, 'Allan Juma', 'allan.juma@aben.co.ke', '123 Kasarani.');
